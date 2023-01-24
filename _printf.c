c004d3ac6f7906d2581dc5f5d861dbf3ddb8f29a
CodeXii <78667460+codeXii@users.noreply.github.com>
2023-01-24 14:48:45 -0800

    _printf

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _printf - is a function that selects the correct function to printout to stdout.
- * @format: the right identifier to look for.
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
  * Return: the length of the string.
  */
 int _printf(const char * const format, ...)
@@ -16,29 +16,29 @@ int _printf(const char * const format, ...)
 	};
 
 	va_list args;
-	int a = 0, b, len = 0;
+	int i = 0, j, len = 0;
 
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
 Here:
-	while (format[a] != '\0')
+	while (format[i] != '\0')
 	{
-		b = 13;
-		while (b >= 0)
+		j = 13;
+		while (j >= 0)
 		{
-			if (m[b].id[0] == format[a] && m[b].id[1] == format[a + 1])
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
 			{
-				len += m[b].f(args);
-				a = a + 2;
+				len += m[j].f(args);
+				i = i + 2;
 				goto Here;
 			}
-			b--;
+			j--;
 		}
-		_putchar(format[a]);
+		_putchar(format[i]);
 		len++;
-		a++;
+		i++;
 	}
 	va_end(args);
 	return (len);
